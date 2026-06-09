b21e325a26b4a36b1125fe889e18fe48af8b9b25
Matteo Concutelli <concutellimatteo@gmail.com>
2026-06-09 16:08:24 +0200

    Fix typo in interceptor initialization function name

--- src/rt/cqmsan/cqmsan_interceptors.cpp
+++ src/rt/cqmsan/cqmsan_interceptors.cpp
@@ -1806,7 +1806,7 @@ void InitializeInterceptors() {
 
   
   //InitializeCommonInterceptors();
-  InitializeMemintrinsicsInterceptors();
+  InitializeMemintrinsicInterceptors();
   InitializeSignalInterceptors();
 
   INTERCEPT_FUNCTION(posix_memalign);
